7ee4029d03dacf374dc46af6723e536b41d7f665
Benjamin Winkler <benjiwinkler@yahoo.com>
2021-05-20 11:57:10 -0500

    basic constructor

---
+++ __tests__/Employee.test.js
@@ -0,0 +1,36 @@
+const { test, expect } = require('@jest/globals');
+const Employee = require('../lib/Employee');
+
+mockInput = {
+    name: 'Dave',
+    id: 212223,
+    email: 'fake@fake.fake',
+    role: 'Employee'
+};
+
+test('creates Employee object', () => {
+    const employee = new Employee(mockInput);
+
+    expect(employee.name).toBe('Dave');
+    expect(employee.id).toBe(212223);
+    expect(employee.enail).toBe('fake@fake.fake');
+    expect(employee.role).toBe('Employee');
+});
+
+test("get employee's id as a number", () => {
+    const employee = new Employee(mockInput);
+
+    expect(employee.getID()).toEqual(expect.any(Number));
+});
+
+test("get employee's email as a string", () => {
+    const employee = new Employee(mockInput);
+
+    expect(employee.getEmail()).toEqual(expect.any(String));
+});
+
+test("get employee's role as a string", () => {
+    const employee = new Employee(mockInput);
+
+    expect(employee.getRole()).toEqual(expect.any(String));
+});
