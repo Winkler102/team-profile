2cb5b8cd6965d9dfe9d67ab5d9e945cfc7511932
Benjamin Winkler <benjiwinkler@yahoo.com>
2021-05-21 13:38:03 -0500

    saving updates

--- src/constructHTML.js
+++ src/constructHTML.js
@@ -1,3 +1,17 @@
+const makeCards = (array) => {
+    return `
+    <div class="card" style="width: 18rem;">
+  <div class="card-body">
+    <h5 class="card-title">Card title</h5>
+    <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" class="card-link">Card link</a>
+    <a href="#" class="card-link">Another link</a>
+  </div>
+</div>
+`;
+}
+
 module.exports = constructHTML = (array) => {
     return `
     <!DOCTYPE html>
@@ -10,10 +24,13 @@ module.exports = constructHTML = (array) => {
       <title>Team Roster</title>
       <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.3.1/css/bootstrap.min.css">
     </head>
-  
+
     <body>
-      <header></header>
-      <main></main>
+      <header class='p-3 bg-info'>
+        <h1>The Team</h1>
+      </header>
+        <main> 
+        </main>
     </body>
     </html>
     `;
